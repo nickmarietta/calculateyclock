ba3bedbd6ee470488b7c268812ad8bf1069904c9
nickmarietta <nick.marietta@csu.fullerton.edu>
2025-04-18 20:52:39 -0700

    updating basic page and implemented react-router

--- CalculateyClock/src/main.jsx
+++ CalculateyClock/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LandingPage from "./pages/LandingPage"
+import './App.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ CalculateyClock/src/pages/LandingPage.jsx
@@ -0,0 +1,8 @@
+
+export default function LandingPage() {
+    return (
+        <div>
+            <h1>Calculate y Clock</h1>
+        </div>
+    );
+    }
