70d87265e9f84645a75e0d8cc964578c7044b3b1
Bryant Martinez <bryant.e.martinez@outlook.com>
2025-04-29 22:16:59 -0700

    updated landing page and fixed footer showing white/ also added more colors

---
+++ CalculateyClock/src/components/FooterBar.jsx
@@ -0,0 +1,9 @@
+export default function FooterBar() {
+  return (
+    <nav className="items-center p-4 bg-[#181C14] text-[#ECDFCC]">
+      <div className="space-x-4">
+      <div className="text-[11px] font-bold">*Reminder This Is Just An Estimate, PayDayZ Is Not Liable For Company Issues Between Your Job And Yourself, Get A Lawyer!</div>
+      </div>
+    </nav>
+  );
+}
--- CalculateyClock/src/pages/LandingPage.jsx
+++ CalculateyClock/src/pages/LandingPage.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/NavBar";
 import GradientBackground from "../components/GradientBackground";
+import FooterBar from "../components/FooterBar";
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="flex flex-col items-center justify-center h-screen p-4 overflow-y-hidden">
+      <main className="flex-grow">
         <GradientBackground>
-          <h1 className="text-3xl font-bold mb-2 text-center text-[#ECDFCC]">About:</h1>
+          <h1 className="text-3xl font-bold p-12 mb-2 text-center text-[#ECDFCC]">About:</h1>
           <p className="text-2xl font-bold mb-6 text-center text-[#ECDFCC]">
             Ever Wondered How Much You'll Really Be Making For Your Current Pay Week? Try Our Paycheck Calculator Estimator, Made Using California's Most Recent 2024* Income Tax Information. 
           </p>
@@ -29,10 +30,16 @@ export default function LandingPage() {
             </button>
           </Link>
           </div>
-          <f className="text-1 italic mb-20 pt-60text-center text-[#ECDFCC]">
-              Reminder This Is Just An Estimate, PayDayZ Is Not Liable For Company Issues Between Your Job And Yourself, Get A Lawyer
-          </f>
-        </GradientBackground>
+          </GradientBackground>
+          </main>
+          <div>
+          <FooterBar>
+          <div className="flex flex-col items-center justify-center h-screen p-4 overflow-y-hidden">
+            <h1 className="text-1 italic mb-20 pt-60text-center text-[#ECDFCC]">
+              *Reminder This Is Just An Estimate, PayDayZ Is Not Liable For Company Issues Between Your Job And Yourself, Get A Lawyer
+          </h1>
+          </div>
+          </FooterBar>
       </div>
     </div>
   );
